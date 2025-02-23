4eacdfda52f49a9e24d03eb3b8565c0f2c75d9ae
Hamilton Moore <hamiltonmoore@pop-os.localdomain>
2025-02-22 18:41:10 -0600

    removed commented out code

--- code/client/src/store/userSlice.ts
+++ code/client/src/store/userSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { User } from '../types'; // Adjust path as needed
+import { User } from '../types';
 
 export interface UserState {
   users: User[];
