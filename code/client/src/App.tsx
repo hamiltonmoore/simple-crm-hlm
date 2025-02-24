127b12b8ed5d65e3da1eb5a745e3662b10c09958
Hamilton Moore <hamiltonmoore@pop-os.localdomain>
2025-02-24 11:58:33 -0600

    made simple crm top left into a link to home page

--- code/client/src/App.tsx
+++ code/client/src/App.tsx
@@ -1,18 +1,22 @@
 import { UserPage } from "./user-page"
 import { Users } from "./users";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export const App: React.FC = () => {
     return (
-        <div className="p-4 space-y-8">
-            <h1 className="text-xl">SimpleCrm</h1>
-            <Router>
+        <Router>
+            <div className="p-4 space-y-8 min-h-screen">
+                <h1 className="text-xl font-bold">
+                    <Link to="/users" className="text-blue-500 hover:text-blue-700">
+                        Simple CRM
+                    </Link>
+                </h1>
                 <Routes>
-                    <Route path="/users" element={<Users/>} />
-                    <Route path="/users/:id" element={<UserPage/>} />
+                    <Route path="/users" element={<Users />} />
+                    <Route path="/users/:id" element={<UserPage />} />
                 </Routes>
-            </Router>
-        </div>
+            </div>
+        </Router>
     );
 };
 
